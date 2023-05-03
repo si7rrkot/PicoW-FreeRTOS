809e3803a566f71d9853a5992b706154bb6a74e6
Yuh <hui.yu@itourshare.com>
2023-05-03 23:41:33 +0800

    chore(*): 🔧 Adjust directory structure

---
+++ main.c
@@ -0,0 +1,16 @@
+#include "pico/stdlib.h"
+#include <FreeRTOS.h>
+#include <stdio.h>
+#include <task.h>
+#include "shell_port.h"
+
+int main()
+{
+    stdio_init_all();
+    printf("pico init stdio all finished\n");
+    xTaskCreate((TaskFunction_t)User_Shell_Init, "Shell", 256, NULL, 1, NULL);
+    vTaskStartScheduler();
+
+    while (1) {
+    };
+}
