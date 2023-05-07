e2b15e1cdb3a9e1cab36c305c226ac410ac04458
Yuh <hui.yu@itourshare.com>
2023-05-07 12:30:55 +0800

    fix(wifi_scan): 🐛 wifi scan can stop

--- service/wifi/src/wifi_scan.c
+++ service/wifi/src/wifi_scan.c
@@ -5,12 +5,11 @@ static int quit_flag = 0;
 
 static int scan_result(void *env, const cyw43_ev_scan_result_t *result)
 {
-    if (result) {
+    if (result->ssid) {
         printf("ssid: %-32s rssi: %4d chan: %3d mac: %02x:%02x:%02x:%02x:%02x:%02x sec: %u\n",
                result->ssid, result->rssi, result->channel,
                result->bssid[0], result->bssid[1], result->bssid[2], result->bssid[3], result->bssid[4], result->bssid[5],
                result->auth_mode);
-        quit_flag = 1;
     }
     return 0;
 }
@@ -29,13 +28,9 @@ int wifi_scan()
     while (true) {
         if (absolute_time_diff_us(get_absolute_time(), scan_time) < 0) {
             if (!scan_in_progress) {
-                if (quit_flag) {
-                    quit_flag = 0;
-                    break;
-                }
                 cyw43_wifi_scan_options_t scan_options = {0};
 
-                int err                                = cyw43_wifi_scan(&cyw43_state, &scan_options, NULL, scan_result);
+                int err = cyw43_wifi_scan(&cyw43_state, &scan_options, NULL, scan_result);
                 if (err == 0) {
                     printf("\nPerforming wifi scan\n");
                     scan_in_progress = true;
@@ -46,6 +41,7 @@ int wifi_scan()
             } else if (!cyw43_wifi_scan_active(&cyw43_state)) {
                 scan_time        = make_timeout_time_ms(10000);   // wait 10s and scan again
                 scan_in_progress = false;
+                break;
             }
         }
         // the following #ifdef is only here so this same example can be used in multiple modes;
